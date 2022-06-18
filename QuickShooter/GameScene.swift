20c28ae112a49b6897a9e34e52385178434463cc
AndresMarq <70426525+AndresMarq@users.noreply.github.com>
2022-06-18 17:25:23 -0600

    Updated logic for collision

--- QuickShooter/GameScene.swift
+++ QuickShooter/GameScene.swift
@@ -41,7 +41,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let bullets = SKSpriteNode(imageNamed: "hammer")
         bullets.size = CGSize(width: 50, height: 50)
         bullets.name = "bullet"
-        bullets.position = location
+        bullets.position = CGPoint(x: 50, y: location.y)
         bullets.physicsBody = SKPhysicsBody(texture: bullets.texture!, size: bullets.size)
         bullets.physicsBody?.velocity = CGVector(dx: 1500, dy: 0)
         bullets.physicsBody?.categoryBitMask = 1
@@ -63,13 +63,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        // Remove the nodes once they are invisible
+        for node in children {
+            if node.position.x < -300 || node.position.y < -300 {
+                node.removeFromParent()
+            }
+        }
     }
     
     @objc func createTarget() {
+        // Assigns either a good or a bad penguin
         guard let target = targets.randomElement() else { return }
         
         let sprite = SKSpriteNode(imageNamed: target)
         sprite.position = CGPoint(x: Int.random(in: 400...1000), y: 720)
+        sprite.name = target
         addChild(sprite)
         
         sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
@@ -78,10 +86,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func collisionBetween(bullet: SKNode, target: SKNode) {
+        // Update score based on whether good or bad penguin was hitted
+        if target.name == "penguinGood" {
+            score += 1
+        } else if target.name == "penguinEvil"{
+            score -= 1
+        }
+        // Call destroy for both target and bullet
         destroy(bullet: bullet, target: target)
     }
     
     func destroy(bullet: SKNode, target: SKNode) {
+        let explosion = SKEmitterNode(fileNamed: "explosion")!
+        explosion.position = target.position
+        addChild(explosion)
+        
         bullet.removeFromParent()
         target.removeFromParent()
     }
